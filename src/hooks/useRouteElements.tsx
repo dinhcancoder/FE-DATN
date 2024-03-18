6f4ec7a06ab5096ba5e3533235216f8b401f7297
kanisdev <dinhcan.cntt@gmail.com>
2024-03-18 19:34:55 +0700

    FE-DATN

---
+++ src/hooks/useRouteElements.tsx
@@ -0,0 +1,33 @@
+import { useRoutes } from 'react-router-dom'
+import MainLayout from '~/layouts/MainLayout'
+import Home from '~/pages/Home'
+import Login from '~/pages/Login'
+import Register from '~/pages/Register'
+
+function useRouteElements() {
+  const routeElements = useRoutes([
+    {
+      path: '/',
+      index: true,
+      element: (
+        <MainLayout>
+          <Home />
+        </MainLayout>
+      )
+    },
+    {
+      path: '/login',
+      index: true,
+      element: <Login />
+    },
+    {
+      path: '/register',
+      index: true,
+      element: <Register />
+    }
+  ])
+
+  return routeElements
+}
+
+export default useRouteElements
